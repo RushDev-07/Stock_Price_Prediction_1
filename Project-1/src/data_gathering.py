8909b7d088187123553fb3e41a1f55a23af65294
Rushabh Mehta <154753885+RushDev-07@users.noreply.github.com>
2024-11-04 16:36:10 +0530

    Delete Project-1/src directory

--- Project-1/src/data_gathering.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-def fetch_stock_data(ticker, start, end,interval):
-    data = yf.download(ticker, start="2014-01-01", end="2024-01-01", interval='1d')
-    return data
--- Project-1/src/feature_engineering.py
+++
@@ -1,8 +0,0 @@
-def add_technical_indicators(data):
-    data['SMA_10'] = data['Close'].rolling(window=10).mean()
-    data['EMA_10'] = data['Close'].ewm(span=10, adjust=False).mean()
-    data['BB_Middle'] = data['Close'].rolling(window=20).mean()
-    data['BB_Upper'] = data['BB_Middle'] + 2 * data['Close'].rolling(window=20).std()
-    data['BB_Lower'] = data['BB_Middle'] - 2 * data['Close'].rolling(window=20).std()
-    # Add more indicators as needed
-    return data
