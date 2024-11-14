0923d363cb485f1483bf91df7e8c565df48c8dbc
Rushabh Mehta <154753885+RushDev-07@users.noreply.github.com>
2024-11-14 15:14:55 +0530

    Delete Project-1 directory

--- Project-1/tests/linting/run_code_quality_checks.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def test_code_quality():
-    os.system("pylint src/ --exit-zero")
-    os.system("flake8 src/")
--- Project-1/tests/performance/load_test_backend.py
+++
@@ -1,6 +0,0 @@
-from locust import HttpUser, task
-
-class LoadTestUser(HttpUser):
-    @task
-    def run_prediction(self):
-        self.client.post("/run_pipeline", json={"ticker": "AAPL"})
